1fa98946b4cc1a52afe4d9b5c0eeb0d7d4904d07
Urbano Jr <urbanobazz@gmail.com>
2024-06-20 18:49:26 +0200

    a coupl changes made, still not working

--- src/requirements/mariadb/tools/setup.sh
+++ src/requirements/mariadb/tools/setup.sh
@@ -13,14 +13,16 @@
 # Start the database with the command passed to the script
 #exec $@
 
+set -e
+
 service mariadb start
 
 mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');
 EOF
 
 sleep 5
--- src/requirements/wordpress/conf/wd-setup.sh
+++ src/requirements/wordpress/conf/wd-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cd /var/www/html
 
@@ -6,13 +7,6 @@ cd /var/www/html
 if ! wp core is-installed --allow-root; then
 	echo "Downloading Wordpress..."
 	wp core download --allow-root
-	wp core install \
-	--url=${DOMAIN_NAME} \
-	--title=${WP_TITLE} \
-	--admin_user=${WP_ADMIN} \
-	--admin_password=${WP_ADMIN_PASSWORD} \
-	--admin_email=${WP_ADMIN_EMAIL}
-	--allow-root
 else
 	echo "Wordpress already installed"
 fi
@@ -20,16 +14,23 @@ fi
 #Check if wp-config.php exists
 if [! -f wp-config.php]; then
 	echo "Creating wp-config.php..."
-	wp core config create \
+	wp config create \
 	--dbname=${DB_NAME} \
 	--dbuser=${DB_USER} \
 	--dbpass=${DB_PASSWORD} \
 	--dbhost=${WP_DB_HOST} \
 	--allow-root
+	wp core install \
+	--url=${DOMAIN_NAME} \
+	--title=${WP_TITLE} \
+	--admin_user=${WP_ADMIN} \
+	--admin_password=${WP_ADMIN_PASSWORD} \
+	--admin_email=${WP_ADMIN_EMAIL} \
+	--allow-root
 fi
 
 # create user
-if !wp user list --allow-root | grep -q $WP_GUEST_USER; then
+if ! wp --allow-root --path="/var/www/html" user get ${WP_USER}; then
 	echo "Creating user..."
 	wp user create \
 	${WP_USER} ${WP_USER_EMAIL} --role=editor \
